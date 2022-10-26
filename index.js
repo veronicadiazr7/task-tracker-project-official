a94773975501fe5932170c86c06ffb427fc947da
veronicadiazr7 <veronica.ddiaz7@gmail.com>
2022-10-25 23:03:21 -0400

    Changed port to 80 the default HTTP port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 7000;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
